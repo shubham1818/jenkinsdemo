9d6107ad0d8543ac42184a141fe279af1196ded1
Shubham <shubhamadamane@gmail.com>
2020-11-21 16:48:13 +0530

    commit 6

--- Hi.java
+++ Hi.java
@@ -1,7 +1,7 @@
 public class Hi{
 
 public static void main (String []args) {
-
+int x = 1/0;
 System.out.println("Hello Shubham");
 System.out.println("Bye Shubham");
 System.out.println("Build 3");
